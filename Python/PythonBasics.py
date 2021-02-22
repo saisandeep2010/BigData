7d23a503155c42c18137b11bde512ff98ad9b5f9
Sandeep <saisandeep.2010@gmail.com>
2021-02-22 11:56:14 +0530

    python learning

---
+++ Python/PythonBasics.py
@@ -0,0 +1,6 @@
+# Maps and Filters are higher order functions that take a function as input argument along iterible
+
+"""Generators:
+Generators are a type of iterable, like lists or tuples.
+Unlike lists, they don't allow indexing with arbitrary indices, but they can still be iterated through with for loops. 
+They can be created using functions and the yield statement.."""
